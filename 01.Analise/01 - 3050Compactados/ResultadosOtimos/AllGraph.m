83e9e419563d7a11a75f6284d71da3b63f604897
shizde <rafaelperez.dev@gmail.com>
2021-12-01 17:20:12 +0000

    Add files via upload

---
+++ 01.Analise/01 - 3050Compactados/ResultadosOtimos/AllGraph.m
@@ -0,0 +1,23 @@
+
+
+clear; close all; Graph('resultadosNMEAmD_30pts_ZigZag1');
+clear; close all; Graph('resultadosNMEAmD_30pts_ZigZag2');
+
+clear; close all; Graph('resultadosNMEAmD_35pts_7km');
+clear; close all; Graph('resultadosNMEAmD_35pts_15km');
+clear; close all; Graph('resultadosNMEAmD_35pts_60km');
+clear; close all; Graph('resultadosNMEAmD_35pts_Per');
+clear; close all; Graph('resultadosNMEAmD_35pts_ZigZag');
+
+clear; close all; Graph('resultadosNMEApD_21pts_ZigZag1');
+clear; close all; Graph('resultadosNMEApD_21pts_ZigZag2');
+
+clear; close all; Graph('resultadosNMEApD_50pts_1km');
+clear; close all; Graph('resultadosNMEApD_50pts_2km');
+clear; close all; Graph('resultadosNMEApD_50pts_5km');
+clear; close all; Graph('resultadosNMEApD_50pts_Per');
+clear; close all; Graph('resultadosNMEApD_50pts_ZigZag');
+
+
+
+
