aa54f62979244b207dae39462c8e74816a3210fd
shizde <rafaelperez.dev@gmail.com>
2021-12-01 17:09:55 +0000

    Add files via upload

---
+++ 04.Prototipo/GNSS_IoT_TestNode/Teste_SD/Debug/SPI/SPI.cpp.d
@@ -0,0 +1,23 @@
+C:\Users\scorr\AppData\Local\Temp\VMBuilds\IotNode\esp32_firebeetle32\Debug\SPI\SPI.cpp.o: \
+ C:\Users\scorr\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.5\libraries\SPI\src\SPI.cpp \
+ C:\Users\scorr\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.5\libraries\SPI\src\SPI.h \
+ C:\Users\scorr\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.5/tools/sdk/include/newlib/stdlib.h \
+ C:\Users\scorr\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.5/tools/sdk/include/newlib/machine/ieeefp.h \
+ C:\Users\scorr\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.5/tools/sdk/include/newlib/_ansi.h \
+ C:\Users\scorr\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.5/tools/sdk/include/newlib/newlib.h \
+ C:\Users\scorr\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.5/tools/sdk/include/newlib/sys/config.h \
+ C:\Users\scorr\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.5/tools/sdk/include/newlib/sys/features.h \
+ C:\Users\scorr\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.5/tools/sdk/include/esp32/xtensa/config/core-isa.h \
+ C:\Users\scorr\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.5/tools/sdk/include/newlib/sys/reent.h \
+ C:\Users\scorr\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.5/tools/sdk/include/newlib/_ansi.h \
+ C:\Users\scorr\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.5/tools/sdk/include/newlib/sys/_types.h \
+ C:\Users\scorr\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.5/tools/sdk/include/newlib/machine/_types.h \
+ C:\Users\scorr\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.5/tools/sdk/include/newlib/machine/_default_types.h \
+ C:\Users\scorr\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.5/tools/sdk/include/newlib/sys/lock.h \
+ C:\Users\scorr\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.5/tools/sdk/include/newlib/sys/cdefs.h \
+ C:\Users\scorr\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.5/tools/sdk/include/newlib/machine/stdlib.h \
+ C:\Users\scorr\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.5/tools/sdk/include/newlib/alloca.h \
+ C:\Users\scorr\AppData\Local\ARDUIN~1\packages\esp32\hardware\esp32\10A26C~1.5\variants\FIREBE~1/pins_arduino.h \
+ C:\Users\scorr\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.5/tools/sdk/include/newlib/stdint.h \
+ C:\Users\scorr\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.5/tools/sdk/include/newlib/sys/_intsup.h \
+ C:\Users\scorr\AppData\Local\ARDUIN~1\packages\esp32\hardware\esp32\10A26C~1.5\cores\esp32/esp32-hal-spi.h
