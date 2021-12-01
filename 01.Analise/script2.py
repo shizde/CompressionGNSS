76509dff9b511f488e79ed2ed31baa3d92c76fd8
shizde <rafaelperez.dev@gmail.com>
2021-12-01 17:17:08 +0000

    Add files via upload

---
+++ 01.Analise/script2.py
@@ -0,0 +1,56 @@
+import functools
+import operator
+import datetime
+import random
+import math
+import csv
+import re
+import os
+
+# Definitions
+location = '/home/rperez/Dropbox/01 - PRafael/01 - Dados/04 - Simulacao/03 - AnalisadorLZ77/03 - 100ocompactados/FicheirosCompactados'
+files_in_dir = []
+
+# r=>root, d=>directories, f=>files
+for r, d, f in os.walk(location):
+    for item in f:
+        if '.json' in item:
+            files_in_dir.append(os.path.join(r, item))
+
+
+def remover_lz77():
+    # Definitions
+    location = '/home/rperez/Dropbox/01 - PRafael/01 - Dados/04 - Simulacao/03 - AnalisadorLZ77/03 - 100ocompactados/FicheirosCompactados'
+    files_in_dir = []
+
+    # r=>root, d=>directories, f=>files
+    for r, d, f in os.walk(location):
+        for item in f:
+            if '.json' in item:
+                files_in_dir.append(os.path.join(r, item))
+
+    for item in files_in_dir:
+        if item[-5:] == '.lz77':
+            command = 'rm ' + item[122:]
+            os.system(command)
+
+os.system('rm resultados.csv')
+with open("resultados.csv", mode='a') as csv_file:
+            for item in files_in_dir:
+                if item[-5:] == '.json':
+                    lz77 = item[122:] + '.lz77'
+                    lz77_command = './lz77 -c -i '+item[122:]+' -o '+lz77
+                    os.system(lz77_command)
+
+                    #Filename
+                    csv_file.write(item[122:]+',')
+                    #Original Size
+                    file_size_original = os.path.getsize(item[122:])
+                    csv_file.write(str(file_size_original)+',')
+                    #LZ77 Size
+                    file_size_lz77 = os.path.getsize(lz77)
+                    csv_file.write(str(file_size_lz77)+',')
+                    #Close line
+                    csv_file.write('\n')
+                    #print('Name:', item[122:], 'O.Size:',file_size_original,'LZ77:',file_size_lz77,'I:',i,'J:',j)
+            remover_lz77()
