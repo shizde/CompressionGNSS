d6a1dcf10b454c0049241b6e4b328f2a88e956ae
shizde <rafaelperez.dev@gmail.com>
2021-12-01 17:13:43 +0000

    Add files via upload

---
+++ 04.Prototipo/GNSS_IoT_TestNode/Teste_SD/Release/Adafruit_GFX_Library/glcdfont.c.d
@@ -0,0 +1,2 @@
+C:\Users\scorr\AppData\Local\Temp\VMBuilds\IotNode\esp32_firebeetle32\Release\Adafruit_GFX_Library\glcdfont.c.o: \
+ C:\Users\scorr\OneDrive\Documentos\Arduino\libraries\Adafruit_GFX_Library\glcdfont.c
