6e7b1ea38f1b9d74337a354f7caa74e9ddf361b7
shizde <rafaelsperez@vivaldi.net>
2021-12-01 16:50:50 +0000

    Delete 04.Prototipo/GNSS_IoT_TestNode directory

--- 04.Prototipo/GNSS_IoT_TestNode/Teste_SD/Debug/Adafruit_GFX_Library/glcdfont.c.d
+++
@@ -1,2 +0,0 @@
-C:\Users\rafam\AppData\Local\Temp\VMBuilds\Teste_SD\esp32_firebeetle32\Debug\Adafruit_GFX_Library\glcdfont.c.o: \
- C:\Users\rafam\OneDrive\Documentos\Arduino\libraries\Adafruit_GFX_Library\glcdfont.c
